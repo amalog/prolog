b18768a1bb729bc950e779e744b873aec12f0165
Michael Hendricks <michael@ndrix.org>
2015-01-03 09:36:35 -0700

    Always send bin/ama output through a pager
    I suspect that most users will configure their pager to only page
    content that's too large for a single screen.  Users who don't like this
    can set their pager to 'cat'.  Of course, I'm the only user at the
    moment :-)

--- bin/ama.pl
+++ bin/ama.pl
@@ -4,10 +4,53 @@
 
 :- use_module(library(amalog), []).
 :- use_module(library(pprint), [print_term/2]).
+:- use_module(library(process)).
 
 main([Filename]) :-
     once( amalog:program(file(Filename),Program)
         ; throw("Unable to parse program")
         ),
-    print_term(Program,[indent_arguments(4)]),
+    with_pager(pretty_print(Program)),
     nl.
+
+
+% pretty print an Amalog program AST
+pretty_print(Program) :-
+    print_term(
+        Program,
+        [
+            indent_arguments(4),
+            output(current_output)
+        ]
+    ).
+
+
+% send output through the user's preferred pager
+:- meta_predicate with_pager(0).
+with_pager(Goal) :-
+    setup_call_cleanup(
+        with_pager_setup(PagerIn,Pid),
+        with_output_to(PagerIn,Goal),
+        with_pager_cleanup(PagerIn,Pid)
+    ).
+
+
+% set up a pager
+with_pager_setup(PagerIn,Pid) :-
+    once( getenv('PAGER',Pager)
+        ; Pager=less
+        ),
+    process_create(
+        path(Pager),
+        [],
+        [
+            stdin(pipe(PagerIn)),
+            stdout(std),
+            process(Pid)
+        ]
+    ).
+
+% clean up after a pager
+with_pager_cleanup(PagerIn,Pid) :-
+    close(PagerIn),
+    process_wait(Pid,_Status).
