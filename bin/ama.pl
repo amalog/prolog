d8d11a6f689d6af13816281c2ff6f9351a93919c
Michael Hendricks <michael@ndrix.org>
2015-01-03 08:59:44 -0700

    Teach bin/ama to do some formatting
    Piggyback on SWI-Prolog's term formatting for now.  We're just dumping a
    Prolog rendering of Amalog source, so this works pretty well.

--- bin/ama.pl
+++ bin/ama.pl
@@ -3,10 +3,11 @@
 :- user:asserta(file_search_path(library,prolog)).
 
 :- use_module(library(amalog), []).
+:- use_module(library(pprint), [print_term/2]).
 
 main([Filename]) :-
     once( amalog:program(file(Filename),Program)
         ; throw("Unable to parse program")
         ),
-    writeq(Program),
+    print_term(Program,[indent_arguments(4)]),
     nl.
